122ee74e77715c6c30fbf1118dfcba32749bd617
LissethReinoso <LissethReinoso30@users.noreply.github.com>
2020-05-27 00:37:27 -0500

    "Interfaz Gráfica 3"

--- src/ec/edu/ups/vista/VentanasTelefono.java
+++ src/ec/edu/ups/vista/VentanasTelefono.java
@@ -20,63 +20,44 @@ import java.awt.TextField;
 public class VentanasTelefono {
 
     public void VentanasT() {
+        VentanaTelefono ventana = new VentanaTelefono("TELEFONOS", 400, 100);
+        //Creamos paneles
+        Panel panelGeneral = new Panel();
+        Panel panel1 = new Panel();
+        Panel panel2 = new Panel();
+
         
-        //Creamos el panel 
-        VentanaTelefono ventana=new VentanaTelefono("TELEFONOS", 400,200);
-        Panel panelGeneral=new Panel();
-        Panel panel1=new Panel();
-        Panel panel2=new Panel();
-        
-       //GridLayout p=new GridLayout(2,2);
-       //Para dimensionar el panel con borderLayout
-      // BorderLayout orden=new BorderLayout();
-       
-       
-       //Crear botones
-       Button boton1=new Button("Registrar los telefonos");
-       Button boton2=new Button("Salir");
-       //FlowLayout posicionar botones
-     //  FlowLayout opcionesMenu=new FlowLayout();
-       
-       
-       
-       //Introducir un texto que no se puede modificar
-      Label titulo= new Label("MENÚ");
-      
-        //Permite la edición de un texto
-      // TextField textot1=new TextField();
-       
-       //textot1.setText("Introduzca el número:");
-       
-       
-       //añade el panel a a la ventana
-       //panel1.setLayout(orden);
-       //panel1.setLayout(orden);
-       
-       //Añadir dimensionalidad al panel
-       
-       ventana.add(panelGeneral);
-       panelGeneral.add(panel1);
-       panelGeneral.add(panel2);
-       
-      // panelGeneral.add(titulo);
-       //añadir botones al panel 
-       panel1.add(boton1);
-       //panelGeneral.add(boton2,BorderLayout.EAST);
-       panel2.add(boton2);
-       
-       
-       
+        //Crear botones
+        Button boton1 = new Button("Registrar los telefonos");
+        Button boton2 = new Button("Salir");
+
+        //Introducir un texto que no se puede modificar
+        Label titulo = new Label("MENÚ:");
+        Label n = new Label("  ");
+
+        //Añadimos el texto al panel
+        panelGeneral.add(titulo);
+        panelGeneral.add(n);
+
+        //Añadir los paneles a la ventana
+        ventana.add(panelGeneral);
+        panelGeneral.add(panel1);
+        panelGeneral.add(panel2);
+
+        //añadir botones al panel 
+        panel1.add(boton1);
+        panel2.add(boton2);
+
     }
-    
-    public void VentanaT2(){
-        VentanaTelefono ventana=new VentanaTelefono("REGISTRAR TELEFONOS", 400,200);
+
+    public void VentanaT2() {
+        VentanaTelefono ventana = new VentanaTelefono("REGISTRAR TELEFONOS", 400, 200);
         //Paneles
+        Panel panelGneral = new Panel();
+        Panel panel3 = new Panel();
+        Panel panel4 = new Panel();
         
-        
+
     }
-    
-   
-   
-    
+
 }
