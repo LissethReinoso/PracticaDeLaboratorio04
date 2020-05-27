bfca360e4a1c892329f7e9a7bbcf346490c2b629
LissethReinoso <LissethReinoso30@users.noreply.github.com>
2020-05-26 21:31:07 -0500

    "Interfaz Gráfica 1"

---
+++ src/ec/edu/ups/vista/VentanaTelefono.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.vista;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author Lisseth Reinoso
+ */
+public class VentanaTelefono extends Frame {
+
+   //Creamos la ventana
+    
+    public VentanaTelefono(String titulo, int ancho, int alto){
+        this.setTitle(titulo);
+        this.setSize(ancho,alto);
+        this.setLocation(200,400);
+        this.setVisible(true);
+    }
+}
---
+++ src/ec/edu/ups/vista/VentanasTelefono.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.vista;
+
+import java.awt.Button;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
+
+/**
+ *
+ * @author Lisseth Reinoso
+ */
+public class VentanasTelefono {
+
+    public void VentanasT() {
+        //Creamos el panel 
+        VentanaTelefono ventana=new VentanaTelefono("TELEFONOS",600,400);
+        Panel panel=new Panel();
+        
+       GridLayout p=new GridLayout();
+       
+       Button[] boton=new Button[2];
+       Label[] label= new Label[10];
+    }
+    
+   
+   
+    
+}
